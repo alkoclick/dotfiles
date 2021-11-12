99da718453d78e105e81c8d13f768ecd7b1395d0
alexander.papageorgiou <alexander.papageorgiou@zivver.com>
2021-11-12 21:13:45 +0100

    Initial commit and setup

---
+++ .bash_aliases
@@ -0,0 +1,3 @@
+alias k=kubectl
+alias qmit='git add . && git commit -m "Committed via qmit alias" && git push'
+alias test_alias='echo "Aliases loaded successfully!"'
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Minimal dependencies for comfortably using a terminal
+echo "Installing CLI dependencies via Homebrew..."
+brew tap homebrew/cask-fonts && brew install --cask font-fira-code-nerd-font
+brew install starship
+
+if grep -q "source ~/.bashrc_addons" ~/.bashrc
+then
+  echo "Not adding source as it already exists"   
+else
+  echo "source ~/.bashrc_addons" > ~/.bashrc
+fi
+
+# Symlink stuff
+echo "Symlinking dotfiles..."
+ln -sf "$PWD/.bashrc_addons" ~/.bashrc_addons
+ln -sf "$PWD/.bash_aliases" ~/.bash_aliases
+
+# Source to see the results
+
+echo "All done, sourcing so you can see results :)"
+source ~/.bashrc
