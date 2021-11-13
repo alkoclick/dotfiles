c279c3704780e546138f18a2a95be25898141c5e
Alexandros Papageorgiou <alkoclick@gmail.com>
2021-11-13 01:20:26 +0100

    Remove now unused font cask because it was MacOS only

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 # Minimal dependencies for comfortably using a terminal
 echo "Installing CLI dependencies via Homebrew..."
-brew tap homebrew/cask-fonts && brew install --cask font-fira-code-nerd-font
 brew install starship
 
 if grep -q "source ~/.bashrc_addons" ~/.bashrc
