194f53775a6d4287c45d697293d64d86bb9a3df6
Alexandros Papageorgiou Koufidis <alkoclick@gmail.com>
2021-11-14 04:17:24 +0100

    Remove snap requirements, add a docker alias

--- .bash_aliases
+++ .bash_aliases
@@ -6,6 +6,9 @@ alias qmit='git add . && git commit -m "Committed via qmit alias" && git push'
 
 # General sysadmin stuff
 # This one requires root or a role that can add/remove snaps
-remove_old_snaps='snap list --all | while read snapname ver rev trk pub notes; do if [[ $notes = *disabled* ]]; then snap remove "$snapname" --revision="$rev"; fi; done'
+alias remove_old_snaps='snap list --all | while read snapname ver rev trk pub notes; do if [[ $notes = *disabled* ]]; then snap remove "$snapname" --revision="$rev"; fi; done'
+
+# DevOpsLand
+alias drun="docker run --rm -it"
 
 alias test_alias='echo "Aliases loaded successfully!"'
--- ubuntu_deps.sh
+++ ubuntu_deps.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+# Eget first so we can do everything else more easily
+curl -o eget.sh https://zyedidia.github.io/eget.sh
+echo "0e64b8a3c13f531da005096cc364ac77835bda54276fedef6c62f3dbdc1ee919  eget.sh" | sha256sum -c || { echo "Checksum validation failed" && exit 1; }
+chmod +x eget.sh && ./eget.sh && rm eget.sh
+chmod +x eget && sudo mv eget /usr/local/bin/
+
+
 # Terminal and looks
-sudo apt-get install fonts-firacode xclip
+sudo apt-get install -y fonts-firacode xclip
 
-curl -o starship_install.sh -sSL "https://starship.rs/install.sh" \
-  && chmod +x starship_install.sh \
-  && ./starship_install.sh -y
-# We don't remove the file at the end, so we can track what we actually run
-
-snap install micro --classic
+sudo eget --to=/usr/local/bin/ "https://github.com/starship/starship/releases/download/v1.0.0/starship-x86_64-unknown-linux-gnu.tar.gz" --verify-sha256=8952efbcbdf8487273d7467caa3ff87b8917ba00fec1a083a36e9e04a6df5cd3
 
 
 # Tooling
 
 ## Lazygit because default git cli is pain
-curl -o lazygit.tar.gz -sSL https://github.com/jesseduffield/lazygit/releases/download/v0.31.3/lazygit_0.31.3_Linux_x86_64.tar.gz
-tar -xvf lazygit.tar.gz lazygit
-sudo mv lazygit /usr/local/bin/
+sudo eget --tag v0.31.3 --to=/usr/local/bin/ jesseduffield/lazygit --verify-sha256=78eff8d126178594a06107c8faff7f27343f4e63281d14fbbc62f6fbb38e5110
+
+## Micro because nano is not enough
+## We have to use this form because there are 2 choices (static binary vs linked) and we'd get an interactive prompt
+sudo eget --to=/usr/local/bin/ "https://github.com/zyedidia/micro/releases/download/v2.0.10/micro-2.0.10-linux64.tar.gz"
