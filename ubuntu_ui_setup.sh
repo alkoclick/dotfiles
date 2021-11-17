4bd69f0e01dc4d5fa4a729cf9fd4e09067294e17
Alexandros Papageorgiou Koufidis <alexander.papageorgiou@zivver.com>
2021-11-17 13:51:35 +0100

    Remove budgie, too much hassle

--- ubuntu_ui_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get --no-install-recommends install ubuntu-budgie-desktop\
-  &&\
-  sudo apt-get autoremove -y
-
-echo 'Budgie has added some crap to your bashrc, remove that manually'
-echo 'Also your Chrome-based browser will have an ugly-ass title bar. Go to brave://settings/appearance and deselect "Use system title bar and borders"'
