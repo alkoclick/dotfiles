d9e6755770e24ebfa9e1d9eb3760e02ade014066
Alexandros Papageorgiou Koufidis <alkoclick@gmail.com>
2021-12-03 23:46:05 +0100

    Use terraform for files instead of symlinking
    Signed-off-by: Alexandros Papageorgiou Koufidis <alkoclick@gmail.com>

---
+++ files_copy.tf
@@ -0,0 +1,17 @@
+locals {
+  files = [
+    ".bashrc",
+    ".bash_aliases",
+    ".fzf.bash",
+    ".gitconfig",
+    ".config/micro/bindings.json",
+    ".config/micro/settings.json",
+    ".config/starship.toml",
+  ]
+}
+
+resource "local_file" "copy_file" {
+  for_each = toset(local.files)
+  content  = file("${path.module}/${each.value}")
+  filename = pathexpand("~/${each.value}")
+}
