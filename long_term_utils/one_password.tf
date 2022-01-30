03cd1d0c6843e2e5985c52b15964194a9c6414ed
Alexandros Papageorgiou Koufidis <alexandros.papageorgiou.koufidis@vanmoof.com>
2022-01-30 22:21:25 +0100

    Split gsettings and dconf into folders

--- long_term_utils/one_password.tf
+++ long_term_utils/one_password.tf
@@ -4,14 +4,11 @@ locals {
 }
 
 resource "shell_script" "install_op" {
-  # Install on long term systems
-  count = var.long_term ? 1 : 0
-
   lifecycle_commands {
     create = format("%s\n%s", file("${path.root}/scripts/install_op.sh"), local.cmd_op_version)
     read   = local.cmd_op_version
     update = format("%s\n%s", file("${path.root}/scripts/install_op.sh"), local.cmd_op_version)
-    delete = "sudo rm /usr/local/bin/op"
+    delete = "sudo rm /usr/local/bin/op || true"
   }
 
   environment = {
--- modules.tf
+++ modules.tf
@@ -6,10 +6,22 @@ module "brew" {
   source = "./brew"
 }
 
+module "dconf" {
+  count = var.long_term ? 1 : 0
+
+  source = "./dconf"
+}
+
+module "gsettings" {
+  count = var.long_term ? 1 : 0
+
+  source = "./gsettings"
+}
+
 module "long_term" {
   count = var.long_term ? 1 : 0
 
-  source = "./long_term"
+  source = "./long_term_utils"
 }
 
 module "keys" {
